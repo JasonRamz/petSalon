56ac1303ebf21a6f97e1a35c0114b3d8a908a117
JasonRamz <12ramirezjason@gmail.com>
2023-09-14 01:01:30 -0700

    class work and contact done for final project

--- scripts/display.js
+++ scripts/display.js
@@ -1,59 +1,24 @@
-function displayCards(){
+function displayTable() {
     let tmp = "";
     //travel the pet arrays **for
     for(let i = 0; i < salon.pets.length; i++){
         let pet = salon.pets[i];
         //create the tmp string ** `<div>${} </div>`
-        tmp=`
-        <div class="pet">
-            <h6>${pet.name}</h6>
-            <h6>${pet.age}</h6>
-            <p>${pet.breed}</p>
-            <h6>${pet.gender}</h6>
-            <p>${pet.service}</p>
-        </div>
+        tmp+=`
+        <tr id="${pet.id}">
+            <td>${pet.name}</td>
+            <td>${pet.age}</td>
+            <td>${pet.breed}</td>
+            <td>${pet.gender}</td>
+            <td>${pet.service}</td>
+            <td>${pet.phone}</td>
+            <td>${pet.payment}</td>
+            <td>
+            <button onclick="deletePet(${pet.id});"><i class="fa-solid fa-trash"></i></button>
+            </td>
+        </tr>
         `;
     //insert into the html ** innerHtml
-    document.getElementById("pets").innerHTML+= tmp;
     }
-}
-
-function displayTable() {
-    // Get the table body element
-    let tableBody = document.getElementById("tables");
-
-    // Clear existing content in the table body
-    tableBody.innerHTML = "";
-
-    // Iterate over the pets array and create rows for each pet
-    for (let i = 0; i < salon.pets.length; i++) {
-        let pet = salon.pets[i];
-
-        // Create a new table row
-        let row = document.createElement("tr");
-
-        // Create table data cells for each pet attribute
-        let nameCell = document.createElement("td");
-        nameCell.textContent = pet.name;
-        row.appendChild(nameCell);
-
-        let ageCell = document.createElement("td");
-        ageCell.textContent = pet.age;
-        row.appendChild(ageCell);
-
-        let breedCell = document.createElement("td");
-        breedCell.textContent = pet.breed;
-        row.appendChild(breedCell);
-
-        let genderCell = document.createElement("td");
-        genderCell.textContent = pet.gender;
-        row.appendChild(genderCell);
-
-        let serviceCell = document.createElement("td");
-        serviceCell.textContent = pet.service;
-        row.appendChild(serviceCell);
-
-        // Append the row to the table body
-        tableBody.appendChild(row);
-    }
+    document.getElementById("table").innerHTML= tmp;
 }
--- scripts/register.js
+++ scripts/register.js
@@ -10,12 +10,16 @@ let salon = {
     pets:[]
 }
 //constructor 
-function Pet(n,a,b,s,g){
+let x=0;
+function Pet(n,a,b,s,g,p,c){
     this.name=n;
     this.age=a;
     this.breed=b;
     this.service=s;
     this.gender=g;
+    this.phone=p;
+    this.payment=c;
+    this.id=x++;
 
 }
 //inputs
@@ -24,6 +28,9 @@ const inputAge = document.getElementById("txtAge");
 const inputBreed = document.getElementById("txtBreed");
 const inputService = document.getElementById("txtService");
 const inputGender = document.getElementById("txtGender");
+const inputPhone = document.getElementById("txtPhone");
+const inputSearch = document.getElementById("btnSearch");
+const inputPayment = document.getElementById("txtPayment");
 //validation
 function isValid(aPet){
     let validation=true;
@@ -32,6 +39,8 @@ function isValid(aPet){
     inputBreed.classList.remove("alert-error");
     inputAge.classList.remove("alert-error");
     inputGender.classList.remove("alert-error");
+    inputPhone.classList.remove("alert-error");
+    inputPayment.classList.remove("alert-error");
 
 
     if(aPet.name==""){
@@ -54,6 +63,10 @@ function isValid(aPet){
         validation=false;
         alert("Please add a pet gender");
     }
+    if(aPet.phone==""){
+        validation=false;
+        alert("Please add a pet phone");
+    }
     if(aPet.name==""){
         validation=false;
         inputName.classList.add("alert-error");
@@ -61,17 +74,18 @@ function isValid(aPet){
         inputGender.classList.add("alert-error");
         inputBreed.classList.add("alert-error");
         inputService.classList.add("alert-error");
+        inputPhone.classList.add("alert-error");
+        inputPayment.classList.add("alert-error");
     }
     return validation;
 }
 
 function register(){
     //get the values from the html 
-    let newPet = new Pet(inputName.value,inputAge.value,inputBreed.value,inputService.value,inputGender.value);
+    let newPet = new Pet(inputName.value,inputAge.value,inputBreed.value,inputService.value,inputGender.value,inputPhone.value,inputPayment.value);
     
     if(isValid(newPet)==true){//validation if is valid 
         salon.pets.push(newPet);//adding to the array
-        displayCards();
         displayTable();
         clearInput();
     };
@@ -82,14 +96,41 @@ function clearInput(){
     inputBreed.value="";
     inputService.value="";
     inputGender.value="";
+    inputPhone.value="";
+    inputPayment.value="";
+    inputSearch.value="";
+}
+function deletePet(petID){
+    let deleteIndex;
+    for(let i=0;i<salon.pets.length;i++){
+        let pet = salon.pets[i];
+        if(petID==pet.id){
+            deleteIndex=i;
+    }
+}
+    salon.pets.splice(deleteIndex,1);//remove the element from the array
+    document.getElementById(petID).remove();
+}
+function search(){
+    let inputSearchalert=document.getElementById("txtSearch").value;
+    console.log(inputSearch);
+    for(let i=0;i<salon.pets.length;i++){
+        let pet=salon.pets[i];
+
+        if(inputSearch.toLowerCase()===pet.name.toLowerCase()){
+            document.getElementById(pet.id).classList.add("alert-search");
+        }else{
+            document.getElementById(pet.id).classList.remove("alert-search");
+        }
+    }
 }
 
+
 function init(){
-    let pet1 = new Pet("Scooby",60,"Dabe","Grooming","Male");
-    let pet2 = new Pet("chompy",25,"chihuahua","Haircut","Female");
-    let pet3 = new Pet("cookie",5,"dalmatian","NailClipping","Male");
+    let pet1 = new Pet("Scooby",60,"Dane","Grooming","Male","619-222-2222","credit");
+    let pet2 = new Pet("chompy",25,"chihuahua","Haircut","Female","619-222-2222","cash");
+    let pet3 = new Pet("cookie",5,"dalmatian","NailClipping","Male","619-222-2222","debit");
     salon.pets.push(pet1,pet2,pet3);//adding to the array
     displayTable();
-    displayCards();
 }
 window.onload=init;//waiting to render the HTML first
